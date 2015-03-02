48edd0d2a23de29f9c7ebe49697ecde54255a88e
Brett-Kugler <brett.kugler@gdt.com>
2015-03-02 13:38:25 -0600

    Initial add

---
+++ man.py
@@ -0,0 +1,25 @@
+# Project MAN
+# Modular Automation Network
+#
+# Cisco Geekfest Hackathon
+# Austin, TX 2015
+#
+
+import requests
+from mininet.topo import Topo
+from mininet.net import Mininet
+
+class NetBlock:
+
+    def __init__():
+        podHosts = []
+
+    def addHost():
+        pass
+
+    def createPodSwitch():
+        pass
+
+
+if __name__ == "__main__":
+    network = Mininet()
